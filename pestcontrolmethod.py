bba01a64935c3ff3cb25964151f54947fee7d62b
Bjbacani <201980167@psu.palawan.edu.ph>
2024-12-14 19:03:17 +0800

    conti

--- pestcontrolmethod.py
+++ pestcontrolmethod.py
@@ -23,3 +23,30 @@ class pest_control_methods(db.Model):
             "Control_Method_id": self.Control_Method_id
             
         }
+
+@app.route("/pest_control_methods", methods=["GET"])
+def get_pcm():
+    pcm = pest_control_methods.query.limit(100)
+    return jsonify(
+        {
+            "success": True,
+            "data": [pcms.dict() for pcms in pcm]
+        }
+    ), 200
+
+@app.route("/pest_control_methods/<int:id>", methods=['GET'])
+def get_pcms(id):
+    pcms = db.session.get(pest_control_methods, id)
+    if not pcms:
+        return jsonify(
+            {
+                "success": False,
+                "error": " Not found"
+            }
+        ), 400
+    return jsonify(
+        {
+            "success": True,
+            "data": pcms.dict()
+        }
+    ), 200
