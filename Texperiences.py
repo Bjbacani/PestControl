d5dfe30eca11b4bfe2fa76bdb23a393ff6de0f2b
Bjbacani <201980167@psu.palawan.edu.ph>
2024-12-15 17:41:36 +0800

    conti code

--- Texperiences.py
+++ Texperiences.py
@@ -49,3 +49,72 @@ def tex(client):
         assert len(json_data["data"]) == 25
         assert json_data["data"][0]["date"] == "10-10-2037"
         
+def tsp(client):
+        mkp = type('experiences', (object,),{
+            "id":1,
+            "date":"2-2-2000",
+            "product_id":1,
+            "customer_id":1,
+            "experience":"Using the pest products made a noticeable difference in controlling pests.",
+            "dict": lambda self: mtd(self)
+            
+        })()
+        
+        with patch('experiences.db.session.get', return_value=mkp):
+            response = client.get("/experiences/1") 
+            assert response.status_code == 200
+            json_data = json.loads(response.data)
+            assert json_data ["success"] is True
+            assert json_data ["data"] ["id"] == 1
+            assert json_data ["data"] ["experiences"] == "Using the pest products made a noticeable difference in controlling pests."
+    
+def tup(client):
+        
+    mkp = type('experiences', (object,),{
+            "id":1,
+            "date":"2-2-2026",
+            "product_id":1,
+            "customer_id":1,
+            "experience":"Using the pest products made a noticeable difference in controlling pests.",
+            "dict": lambda self: mtd(self)
+            
+    })()
+    
+    update_data = {
+            "id":1,
+            "date":"Updated 10-10-2035",
+            "product_id":1,
+            "customer_id":1,
+            "experience":"Updated New expieriences is new"
+           
+            
+    }
+    
+    with patch('experiences.db.session.get', return_value=mkp):
+        response = client.put("/experiences/1",
+                              data=json.dumps(update_data),
+                              content_type='application/json')
+        assert response.status_code == 200
+        json_data = json.loads(response.data)
+        assert json_data["success"] is True
+        assert json_data["data"]["date"] == "Updated 10-10-2025"
+        assert json_data["data"]["experience"] == "Updated New expieriences is new"
+        
+def td(client):
+    mkp = experiences(
+        id =1,
+        date="10-10-2021",
+        product_id=1,
+        customer_id=1,
+        experience="I found the pest products to be very effective."
+    )
+        
+    with patch('experiences.db.session.get',return_value=mkp):
+        with patch('experiences.db.session.delete') as mkd:
+            response = client.delete("/experiences/1")
+            mkd.assert_called_once_with(mkp)
+            assert response.status_code == 200
+            json_data = json.loads(response.data)
+            assert json_data["Success"] is True
+            assert "DELETED" in json_data["message"]
+                              
